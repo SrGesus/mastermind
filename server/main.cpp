65e103510629e5dd4f30bca7ad53b0770fc24bba
SrGesus <108523575+SrGesus@users.noreply.github.com>
2024-11-25 16:17:32 +0000

    fixed segfault on GS argument reading

--- server/main.cpp
+++ server/main.cpp
@@ -1,33 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "server/GameStorage.hpp"
 #include "server/UDPServer.hpp"
 #include "server/UDPServerParser.hpp"
-#include "server/GameStorage.hpp"
 
 const char *DEFAULT_IP = "localhost";
-const char *DEFAULT_PORT = "58000"; 
+const char *DEFAULT_PORT = "58000";
 
 int main(int argc, char **argv) {
-    const char *ip = DEFAULT_IP;
-    const char *port = DEFAULT_PORT;
+  const char *ip = DEFAULT_IP;
+  const char *port = DEFAULT_PORT;
 
-    bool verbose_flag = false;;
+  bool verbose_flag = false;
+  ;
 
-    for (int i = 1; i < argc; i++) {
-        if (strcmp(argv[i], "-p") == 0) port = argv[++i];
-        else if (strcmp(argv[i], "-v") == 0) { verbose_flag = true;
-        } else {
-            fprintf(stderr, "Usage: %s [-p port] [-v]\n", argv[0]);
-            return 1;
-        }
+  for (int i = 1; i < argc; i++) {
+    if (strcmp(argv[i], "-p") == 0 && i + 1 < argc)
+      port = argv[++i];
+    else if (strcmp(argv[i], "-v") == 0) {
+      verbose_flag = true;
+    } else {
+      fprintf(stderr, "Usage: %s [-p port] [-v]\n", argv[0]);
+      return 1;
     }
+  }
 
-    GameStorage sessions = GameStorage();
-    UDPServer s = UDPServer(ip, port, verbose_flag);
-    UDPServerParser parser = UDPServerParser(&sessions);
+  GameStorage sessions = GameStorage();
+  UDPServer s = UDPServer(ip, port, verbose_flag);
+  UDPServerParser parser = UDPServerParser(&sessions);
 
-    while (1) {s.processRequest(parser);}
+  while (1) {
+    s.processRequest(parser);
+  }
 
-    return 0;
+  return 0;
 }
