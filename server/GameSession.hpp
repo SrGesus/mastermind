0154b2a404126a28ad9d41844f27f4f25e5b2715
SrGesus <108523575+SrGesus@users.noreply.github.com>
2024-12-21 00:03:27 +0000

    f

--- server/GameSession.hpp
+++ server/GameSession.hpp
@@ -165,7 +165,7 @@ class GameSession {
   /// @brief Get game score.
   int score() const { return 
     (TRIALS_NUMBER - _nT + 1) * 
-    (600 - _duration) / 5; 
+    (600 - _duration + 1) / 5; 
   }
 
   /// @brief Generates string representation of played trials.
